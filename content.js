cb5c13963fffd9304dc3111ba24c411b8abd3217
Abeer <abeer23105341114@diu.edu.bd>
2026-04-17 23:43:56 +0600

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,8 @@
+var monitorsActive = false
+
 function mountMonitors() {
+  if (monitorsActive) return
+  monitorsActive = true
   mountTabFocusMonitors()
   mountClipboardMonitors()
   mountScreenshotMonitors()
@@ -8,6 +12,8 @@ chrome.storage.local.get(["student_session"], function(d) {
   if (d.student_session && d.student_session.exam_active) {
     loggerActivate(d.student_session)
     mountMonitors()
+   
+    wasFullscreen = !!(document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement)
   }
 })
 
@@ -16,9 +22,29 @@ chrome.runtime.onMessage.addListener(function(msg) {
     loggerActivate(msg.session || {})
     log("LOGIN_LOGOUT", "Exam started")
     mountMonitors()
+    wasFullscreen = !!(document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement)
+  }
+  if (msg.action === "ENTER_FULLSCREEN") {
+    var el = document.documentElement
+    var req = el.requestFullscreen || el.webkitRequestFullscreen || el.mozRequestFullScreen || el.msRequestFullscreen
+    if (req) {
+      req.call(el).then(function() {
+        wasFullscreen = true
+      }).catch(function() {
+        
+        wasFullscreen = false
+        log("FULLSCREEN_EXIT", "Fullscreen could not be entered or was denied")
+      })
+    } else {
+     
+      wasFullscreen = false
+      log("FULLSCREEN_EXIT", "Fullscreen API not supported on this page")
+    }
   }
   if (msg.action === "EXAM_END") {
     log("LOGIN_LOGOUT", "Exam ended")
     loggerDeactivate()
+    monitorsActive = false
+    wasFullscreen = false
   }
 })
